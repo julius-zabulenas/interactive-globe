187c9407784021564bfe06ddb9ccdb957e5c9121
Julius <jzabulenas@protonmail.com>
2022-09-01 11:12:12 +0300

    add pin to globe based on ip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,49 @@
 import Globe from 'react-globe.gl';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const globeEl = useRef();
+  const [data, setData] = useState({});
+  const coordinates = [
+    {
+      lat: data.latitude,
+      lng: data.longitude
+    }
+  ];
+
+  const onLocationClick = () => {
+    globeEl.current.pointOfView({
+      lat: data.latitude,
+      lng: data.longitude,
+      altitude: 1.5
+    }, 1000);
+  };
+
+  useEffect(() => {
+    // Enable globe auto rotation
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.35;
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://ipgeolocation.abstractapi.com/v1/?api_key=0319992f7f8c4912a46f36217ef46ef2");
+      const data = await response.json();
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Globe
+        ref={globeEl}
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-      // pointsData={data}
+        pointsData={coordinates}
+        onPointClick={() => onLocationClick()}
+        pointLabel={() => data.city}
+        pointRadius={0.5}
       />
     </>
   );
